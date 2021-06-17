ec4d3140bf7ae990bf5bb66a32021afc53891a94
Snehashis Sarkar <36337263+snehashis365@users.noreply.github.com>
2021-06-17 19:39:45 +0000

    Added 2 new commands and improved help message

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# Just A BOT
-A simple bot for discord written in Python
+# This is Just a **BOT**
+Written in _Python_ and trying to keep it simple for the time being here's the list of commands:
+
+```
+$hello -> Says Hello
+
+$bye -> Says Bye
+
+$ping -> Returns Bot Latency in miliseconds
+
+$shorten <url1> <url2> ... -> Shortens provided urls with tinyurl
+
+$help -> Shows this help message
+
+(More Coming Soon)
+```
+### **Invite Link**:_ https://tinyurl.com/yf6t3zky _
--- main.py
+++ main.py
@@ -8,39 +8,59 @@ def getshorturl(url):
   shortener = pyshorteners.Shortener()
   return shortener.tinyurl.short(url)
 
-help_msg = "**This is Just a BOT**\nWritten in _Python_ and trying to keep it simple for the time being here's the list of commands:```\n$hello -> Says Hello\n\n$shorten <url> -> Shortens provided url with tinyurl\n\n$help -> Shows this help message\n\n(More Coming Soon)\n```"
+help_msg = "**This is Just a BOT**\nWritten in _Python_ and trying to keep it simple for the time being here's the list of commands:```\n$hello -> Says Hello\n\n$bye -> Says Bye\n\n$ping -> Returns Bot Latency in miliseconds\n\n$shorten <url1> <url2> ... -> Shortens provided urls with tinyurl\n\n$help -> Shows this help message\n\n(More Coming Soon)\n```\n_Invite Link_:** https://tinyurl.com/yf6t3zky **"
 
 bot = commands.Bot(command_prefix = '$')
+bot.remove_command('help')
 
 @bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(bot))
+    print('We have logged in as {0.user}, '.format(bot), end = '')
+    print(f'Ping : {round(bot.latency * 1000)}ms\nInvite Link: https://tinyurl.com/yf6t3zky')
 
 #Migrated to Bot which is a subclass of Client so not much has changed
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send(f'Hello! {message.author.mention}')
-        print(f'Hello requested from {message.guild.name}')
+@bot.command(pass_context=True)
+async def help(message):
+  await message.send(help_msg)
 
-    if message.content.startswith('$shorten'):
-        url = getshorturl(message.content[9:])
-        await message.channel.send('Shortened URL: {}'.format(url))
-        print(f'Shorten request from {message.guild.name}, {message.author.name} generated: {url}')
+@bot.command(pass_context=True)
+async def hello(message):
+  await message.send(f'Hello! {message.author.mention}')
+  print(f'Hello requested from {message.guild.name}')
 
-    #Help message
-    if message.content.startswith('$help'):
-      await message.channel.send(help_msg)
+@bot.command(pass_context=True)
+async def bye(message):
+  await message.send(f'Bye bye {message.author.mention}!!')
+
+@bot.command(pass_contexr=True)
+async def ping(message):
+  await message.send(f'Pong! in {round(bot.latency * 1000)}ms')
+  print(f'Pinged from Guild : {message.guild.name} ID : {message.guild.id}')
+
+@bot.command(pass_context=True)
+async def shorten(message, *args):
+  if len(args) == 0:
+    await message.send('Please a provide _atleast one link_ to generate short link\n**Syntax**:\n```$shorten link1 ...```')
+    return
+  for arg in args:
+    url = getshorturl(arg)
+    await message.send(f'Shortened {arg} -> {url}\n')
+  print(f'Generated {len(args)} shortened links\nRequested by: {message.guild.name}, {message.author.name}')
+
+#@bot.event
+#async def on_message(message):
+#    To override on_message use the below line to avoid problems
+#    await bot.process_commands(message)
+#    if message.author == bot.user:
+#        return
 
 #Introduces itself after joining a server
 @bot.event
 async def on_guild_join(guild):
     general = find(lambda x: x.name == 'general',  guild.text_channels)
     if general and general.permissions_for(guild.me).send_messages:
-        await general.send(f'**Hello everyone at {guild.name}! Let us all have a great time!!**\n _Please use **$help** for list commands_') 
+        await general.send(f'**Hello @everyone at {guild.name}!** Let us all have a great time!!\n _Please use **$help** for the list commands_') 
 
 TOKEN = os.environ['TOKEN']
 
